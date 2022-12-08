c82f7b449a819f0bb698d83c980b04799bc77f4e
OlangJaq <olangjack@yahoo.com>
2022-12-08 07:20:54 -0800

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * main - start or entry point
+ * Description: prints 'Programming is like building a multilingual puzzle'
+ * Return: zero
+ */
+
+int main(void)
+{
+	puts(Programming is like building a multilingual puzzle);
+	return (0);
+}	
