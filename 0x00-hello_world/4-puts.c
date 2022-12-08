397bd40b88bf45134d607989a1a025be1debd1b3
OlangJaq <olangjack@yahoo.com>
2022-12-08 14:13:38 -0800

    tast 4/5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
-/*
- * main - Entry point
- * Description: prints "\"Programming is like building a multilingual puzzle"
- * Return: Always 0 (0-executed sucessfully)
+/**
+ *main - entry point
+ *Return: 0 always
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
+
