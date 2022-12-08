5573991864e399838910c06a5368a66d1ec303f7
OlangJaq <olangjack@yahoo.com>
2022-12-08 07:47:04 -0800

    put file created without printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 
+
+
 /*
- * main - start or entry point
- * Description: prints 'Programming is like building a multilingual puzzle'
- * Return: zero
+ * main - Entry point
+ * Description: prints "Programming is like building a multilingual puzzle"
+ * Return: Always 0 (0-executed sucessfully)
  */
 
 int main(void)
+
 {
-	puts(Programming is like building a multilingual puzzle);
+
+	puts("Programming is like building a multilingual puzzle\n");
+
 	return (0);
-}	
+}
